08255d0516529736279c6844d850ae669fe47b40
aminamondal07 <69319619+aminamondal07@users.noreply.github.com>
2025-07-18 03:41:07 +0530

    Delete start_container.sh

--- start_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull amina0786/simple-python-flask-app
-
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 amina0786/simple-python-flask-app
