d6a152d3a1ff98794b84ca3b0333689db171637c
aminamondal07 <69319619+aminamondal07@users.noreply.github.com>
2025-07-18 03:34:00 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -4,7 +4,6 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull amina0786/simple-python-flask-app
 
-docker ps  -q --filter "publish=5000" | grep -q . && docker stop $(docker ps -q --filter "publish=5000")
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 amina0786/simple-python-flask-app
